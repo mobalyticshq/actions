c36a7928ea07c08254d4b75a0531dc0d93ba127f
Evgeniy S <es@mobalyticshq.com>
2025-06-16 17:41:01 +0500

    wip

--- static-data-upload-pipeline/dist/index.js
+++ static-data-upload-pipeline/dist/index.js
@@ -53167,6 +53167,17 @@ const core = __importStar(__nccwpck_require__(7484));
 const github = __importStar(__nccwpck_require__(6798));
 const fs_1 = __nccwpck_require__(9896);
 const path = __importStar(__nccwpck_require__(6928));
+const report = {
+    merge: {
+        lostGroups: new Array(),
+        newGroups: new Array(),
+        newGroupNotArray: new Array(),
+        oldGroupNotArray: new Array(),
+        deprecatedEntities: new Map(),
+        duplicatesInNewData: new Map(),
+    }
+};
+const mergedJSON = new Map();
 function getAllFiles(dirPath, arrayOfFiles = []) {
     const entries = (0, fs_1.readdirSync)(dirPath);
     for (const entry of entries) {
@@ -53190,7 +53201,7 @@ async function bootstrapPipeline() {
         const google_spreadsheet_id = core.getInput('google_spreadsheet_id');
         console.log(`Vars:`, game_config, game_specific_tests, credentials_json, tmp_assets_folder, prod_assets_folder, google_spreadsheet_id);
         const context = github.context;
-        const token = core.getInput('token'); // GitHub Token как input
+        const token = core.getInput('token');
         const octokit = github.getOctokit(token);
         const sha = context.sha;
         const { owner, repo } = context.repo;
@@ -53206,24 +53217,72 @@ async function bootstrapPipeline() {
         core.setFailed(error.message);
     }
 }
+function mergeGroups(newGroup, oldGroup, group) {
+    mergedJSON.set(group, new Array());
+    oldGroup.forEach(oldIt => {
+        const match = newGroup.filter(newIt => newIt.id == oldIt.id);
+        if (match.length == 0) {
+            //deprecated
+            if (!report.merge.deprecatedEntities.has(group)) {
+                report.merge.deprecatedEntities.set(group, [oldIt.id]);
+            }
+            else {
+                report.merge.deprecatedEntities.get(group)?.push(oldIt.id);
+            }
+            mergedJSON.get(group)?.push({ ...oldIt, deprecated: true });
+        }
+        else if (match.length > 1) {
+            if (!report.merge.duplicatesInNewData.has(group)) {
+                report.merge.duplicatesInNewData.set(group, [oldIt.id]);
+            }
+            else {
+                report.merge.duplicatesInNewData.get(group)?.push(oldIt.id);
+            }
+        }
+        else {
+            //correct
+            mergedJSON.get(group)?.push(match[0]);
+        }
+    });
+}
 function mergeJSON() {
     console.log('##Merge new static data file with old##');
-    const root = process.cwd();
-    const oldData = (0, fs_1.readFileSync)(path.join(root, 'old_static_data.json'), 'utf8');
-    const newData = (0, fs_1.readFileSync)(path.join(root, 'old_static_data.json'), 'utf8');
-    console.log('oldData:');
-    for (const [key, value] of Object.entries(JSON.parse(oldData))) {
-        console.log(`${key}: ${value}`);
+    try {
+        const root = process.cwd();
+        const oldData = JSON.parse((0, fs_1.readFileSync)(path.join(root, 'old_static_data.json'), 'utf8'));
+        const newData = JSON.parse((0, fs_1.readFileSync)(path.join(root, 'old_static_data.json'), 'utf8'));
+        for (const group of Object.keys(oldData)) {
+            if (newData[group] === undefined) {
+                report.merge.lostGroups.push(group);
+            }
+            else {
+                if (!Array.isArray(oldData[group])) {
+                    report.merge.oldGroupNotArray.push(group);
+                    continue;
+                }
+                if (!Array.isArray(newData[group])) {
+                    report.merge.newGroupNotArray.push(group);
+                    continue;
+                }
+                mergeGroups(newData[group], oldData[group], group);
+            }
+        }
+        for (const group of Object.keys(newData)) {
+            if (oldData[group] === undefined) {
+                report.merge.newGroups.push(group);
+            }
+        }
     }
-    console.log('newData:');
-    for (const [key, value] of Object.entries(JSON.parse(newData))) {
-        console.log(`${key}: ${value}`);
+    catch (error) {
+        core.setFailed(`error during the merge ${error}`);
     }
 }
 async function run() {
     console.log('##Run static data upload pipeline:##');
     await bootstrapPipeline();
     mergeJSON();
+    console.log('##Report:##');
+    console.log(report);
 }
 run();
 
--- static-data-upload-pipeline/src/index.ts
+++ static-data-upload-pipeline/src/index.ts
@@ -3,6 +3,24 @@ import * as github from '@actions/github'
 import { readFileSync,readdirSync,statSync } from 'fs';
 import * as path from 'path'
 
+type Entity = {
+  id:string;
+  slug?:string;
+  deprecated?:boolean;
+}
+
+const report={  
+  merge:{
+    lostGroups:new Array<string>(),
+    newGroups:new Array<string>(),
+    newGroupNotArray:new Array<string>(),
+    oldGroupNotArray:new Array<string>(),
+    deprecatedEntities:new Map<string,Array<string>>(),
+    duplicatesInNewData:new Map<string,Array<string>>(),
+  }
+}
+const mergedJSON = new Map<String,Array<Entity> >();
+
 function getAllFiles(dirPath: string, arrayOfFiles: string[] = []): string[] {
   const entries = readdirSync(dirPath)
 
@@ -30,39 +48,85 @@ async function bootstrapPipeline(){
 
     console.log(`Vars:`,game_config,game_specific_tests,credentials_json,tmp_assets_folder,prod_assets_folder,google_spreadsheet_id);    
 
-    const context = github.context
-    const token = core.getInput('token')  // GitHub Token как input
-    const octokit = github.getOctokit(token)
+    const context = github.context;
+    const token = core.getInput('token');
+    const octokit = github.getOctokit(token);
 
-    const sha = context.sha
-    const { owner, repo } = context.repo
+    const sha = context.sha;
+    const { owner, repo } = context.repo;
 
     const response = await octokit.rest.repos.getCommit({
       owner,
       repo,
       ref: sha,
-    })
+    });
 
-    const files = response.data.files?.map((file) => file.filename)
-    core.info(`Files:\n${files?.join('\n')}`)
+    const files = response.data.files?.map((file) => file.filename);
+    core.info(`Files:\n${files?.join('\n')}`);
 
   } catch (error: any) {
     core.setFailed(error.message)
   }
 }
+
+function mergeGroups(newGroup:Array<Entity>,oldGroup:Array<Entity>,group:string){
+  mergedJSON.set(group,new Array());
+
+  oldGroup.forEach(oldIt => {
+    const match = newGroup.filter(newIt=>newIt.id == oldIt.id);
+    if(match.length == 0){
+      //deprecated
+      if(!report.merge.deprecatedEntities.has(group)){
+        report.merge.deprecatedEntities.set(group,[oldIt.id]);
+      }else{
+        report.merge.deprecatedEntities.get(group)?.push(oldIt.id);
+      }
+      mergedJSON.get(group)?.push({...oldIt,deprecated:true});
+    }else if(match.length > 1){
+      if(!report.merge.duplicatesInNewData.has(group)){
+        report.merge.duplicatesInNewData.set(group,[oldIt.id]);
+      }else{
+        report.merge.duplicatesInNewData.get(group)?.push(oldIt.id);
+      }
+    }else{
+      //correct
+      mergedJSON.get(group)?.push(match[0]);
+    }
+  });
+}
+
 function mergeJSON(){
   console.log('##Merge new static data file with old##');
+  try{
   const root = process.cwd();  
-  const oldData = readFileSync(path.join(root,'old_static_data.json'), 'utf8');
-  const newData = readFileSync(path.join(root,'old_static_data.json'), 'utf8');
+  const oldData = JSON.parse(readFileSync(path.join(root,'old_static_data.json'), 'utf8'));
+  const newData = JSON.parse(readFileSync(path.join(root,'old_static_data.json'), 'utf8'));
   
-  console.log('oldData:')
-  for (const [key, value] of Object.entries(JSON.parse(oldData))) {
-    console.log(`${key}: ${value}`);
+
+  for (const group of Object.keys(oldData)) {
+    if(newData[group] === undefined){
+      report.merge.lostGroups.push(group);
+    }else{
+      if(!Array.isArray(oldData[group])){
+        report.merge.oldGroupNotArray.push(group);
+        continue;
+      }
+      if(!Array.isArray(newData[group])){
+        report.merge.newGroupNotArray.push(group);
+        continue;
+      }
+      mergeGroups(newData[group],oldData[group],group);
+    }
   }
-  console.log('newData:')
-  for (const [key, value] of Object.entries(JSON.parse(newData))) {
-    console.log(`${key}: ${value}`);
+
+  for (const group of Object.keys(newData)) {
+    if(oldData[group] === undefined){
+      report.merge.newGroups.push(group);
+    }
+  }
+
+  }catch(error){
+    core.setFailed(`error during the merge ${error}`)
   }
 }
 
@@ -70,6 +134,8 @@ async function run() {
   console.log('##Run static data upload pipeline:##');
   await bootstrapPipeline();
   mergeJSON();
+  console.log('##Report:##');
+  console.log(report);
 }
 
-run()
+run();
