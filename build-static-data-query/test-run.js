40f197c637968fa1aea4c911155b005cc105b197
Stanislav Ustiuzhanin <su@mobalyticshq.com>
2025-12-04 15:33:43 +0700

    fix tets run for build-static-data-query

--- build-static-data-query/test-run.js
+++ build-static-data-query/test-run.js
@@ -1,7 +1,2 @@
 require('dotenv/config');
-const { run } = require('./dist/index.js');
-
-run().catch((error) => {
-  console.error('Error:', error);
-  process.exit(1);
-});
+require('./dist/index.js');
