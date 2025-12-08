eb15f3d0238f0d879248178f20a29cca0810b734
Stanislav Ustiuzhanin <su@mobalyticshq.com>
2025-12-08 17:11:25 +0700

    wip

---
+++ build-static-data-query/dist/build-static-data-query/src/utils/bucket.utils.d.ts
@@ -0,0 +1,3 @@
+import { Bucket } from '@google-cloud/storage';
+export declare function isFolderExists(bucket: Bucket, folderPath: string): Promise<boolean>;
+//# sourceMappingURL=bucket.utils.d.ts.map
--- build-static-data-query/dist/shared/utils/bucket.utils.d.ts
+++ build-static-data-query/dist/shared/utils/bucket.utils.d.ts
@@ -1,8 +1,5 @@
-interface Bucket {
-    upload(sourcePath: string, options: {
-        destination: string;
-    }): Promise<unknown>;
-}
+import { type Bucket } from '@google-cloud/storage';
+import { DynamicModuleConfig } from '../types/dynamic-modules.types';
 export declare function uploadFileToBucket(bucket: Bucket, sourcePath: string, destination: string, bucketName: string, description?: string): Promise<boolean>;
-export {};
+export declare function downloadConfigFromBucket(bucket: Bucket, env: string, game: string): Promise<DynamicModuleConfig | null>;
 //# sourceMappingURL=bucket.utils.d.ts.map
