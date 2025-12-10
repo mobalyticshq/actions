5f45e4efae02db68e3908f85e2f8616194a66e74
Stanislav Ustiuzhanin <su@mobalyticshq.com>
2025-12-10 14:20:49 +0700

    fix mapping module

---
+++ build-static-data-mapping/dist/build-static-data-mapping/src/upload-build/upload-build.d.ts
@@ -0,0 +1,8 @@
+import { Bucket } from '@google-cloud/storage';
+export interface UploadBuildOptions {
+    bucket: Bucket;
+    env: string;
+    game: string;
+}
+export declare function uploadBuild(options: UploadBuildOptions): Promise<void>;
+//# sourceMappingURL=upload-build.d.ts.map
