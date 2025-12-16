252bfa46987545998477d30465a7ae47d590e798
Stanislav Ustiuzhanin <su@mobalyticshq.com>
2025-12-16 13:49:54 +0700

    ai-builds-url-slug-fix-3

--- build-static-data-query/dist/build-static-data-query/src/steps/8-upload-build/upload-build.d.ts
+++ build-static-data-query/dist/build-static-data-query/src/steps/8-upload-build/upload-build.d.ts
@@ -2,7 +2,7 @@ import { Bucket } from '@google-cloud/storage';
 export interface UploadBuildOptions {
     bucket: Bucket;
     env: string;
-    game: string;
+    gameUrlSlug: string;
     schemaVersion: string;
     cacheVersion: string;
 }
